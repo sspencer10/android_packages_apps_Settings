4b8d61b6128681b5039a8f2aaf3758cb6cff27e5
Alex Cruz <du.alexcruz@gmail.com>
2018-02-15 20:32:51 -0500

    Ensure everything plays nice
    - Make system accent/theme selectors unavailable as well when substratum is detected
    - Make use of the methods in our Utils/fwb
    
    Change-Id: Ied7ddbeb33e427461c78bf14565c7bfb30468ee0
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/display/SystemThemePreferenceController.java
+++ src/com/android/settings/display/SystemThemePreferenceController.java
@@ -16,22 +16,26 @@ package com.android.settings.display;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.provider.Settings;
+import android.text.TextUtils;
 
 import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.internal.util.pixeldust.PixeldustUtils;
 
 import libcore.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SystemThemePreferenceController extends AbstractPreferenceController implements
-        Preference.OnPreferenceChangeListener {
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
 
     private static final String SYSTEM_THEME = "system_theme_style";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private ListPreference mSystemThemeStyle;
 
     public SystemThemePreferenceController(Context context) {
@@ -52,12 +56,17 @@ public class SystemThemePreferenceController extends AbstractPreferenceControlle
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME);
-        int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_THEME, 0);
-        int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
-        mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
-        mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        if (!PixeldustUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+            int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_THEME, 0);
+            int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
+            mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
+            mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        } else {
+            mSystemThemeStyle.setEnabled(false);
+            mSystemThemeStyle.setSummary(R.string.disable_themes_installed_title);
+        }
     }
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
