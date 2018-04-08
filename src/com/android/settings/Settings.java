55d5d83997400ca3c093c547ec3eea321205df2e
Adin Kwok <adin.kwok@carbonrom.org>
2018-04-07 23:07:58 -0700

    Settings: External Smart Pixels fragment launch
    Necessary for Smart Pixels tile.
    Based on change by Mazda--.
    
    Reference commit:
    DirtyUnicorns@c637500b89bfe1b726a92efa358cf48b1a2a3093
    
    Change-Id: I5a1892d8211ac968958395326fbbbfcdfcf7e942
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -167,4 +167,5 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
 
     public static class PixelDustSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SmartPixelsActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -138,6 +138,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.pixeldust.settings.PixelDustSettings;
+import com.pixeldust.settings.fragments.SmartPixels;
 
 public class SettingsGateway {
 
@@ -264,6 +265,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             PixelDustSettings.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -303,5 +305,6 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.PixelDustSettingsActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }
